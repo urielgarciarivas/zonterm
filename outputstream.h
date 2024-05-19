ab77f288e9f44046a7cff087ce1ac6fa00ea06e6
urielgarciarivas <fancygarz4@gmail.com>
2024-05-19 12:51:35 -0600

    Add __zng_use_colors to check whether or not to use colors

--- outputstream.h
+++ outputstream.h
@@ -19,25 +19,36 @@
 #ifndef __ZNG_OUTPUTSTREAM_H__
 #define __ZNG_OUTPUTSTREAM_H__
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
-// To make it bold, add 01; between the brace and the number 
+// To make it bold, add 01; between the brace and the number
 
-#define STDERR_RED(...)         \
-do {                            \
-  fprintf(stderr, "\033[91m");  \
-  fprintf(stderr, __VA_ARGS__); \
-  fprintf(stderr, "\033[0m");   \
-  fflush(stderr);               \
+static bool __zng_use_colors;
+
+__attribute__((constructor))
+static void __zng_outputstream_initialize() {
+  __zng_use_colors = isatty(STDOUT_FILENO)
+                  && getenv("NO_COLOR") == NULL
+                  && getenv("TERM") != NULL;
+}
+
+#define STDERR_RED(...)                              \
+do {                                                 \
+  if (__zng_use_colors) fprintf(stderr, "\033[91m"); \
+  fprintf(stderr, __VA_ARGS__);                      \
+  if (__zng_use_colors) fprintf(stderr, "\033[0m");  \
+  fflush(stderr);                                    \
 } while (0)
 
-#define STDOUT_GREEN(...)       \
-do {                            \
-  fprintf(stdout, "\033[92m");  \
-  fprintf(stdout, __VA_ARGS__); \
-  fprintf(stdout, "\033[0m");   \
-  fflush(stdout);               \
+#define STDOUT_GREEN(...)                            \
+do {                                                 \
+  if (__zng_use_colors) fprintf(stdout, "\033[92m"); \
+  fprintf(stdout, __VA_ARGS__);                      \
+  if (__zng_use_colors) fprintf(stdout, "\033[0m");  \
+  fflush(stdout);                                    \
 } while (0)
 
 #endif // __ZNG_OUTPUTSTREAM_H__
